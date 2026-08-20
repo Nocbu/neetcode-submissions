85fd28704d41476eca66fec88b52f90a687c3256
Kishan <147524472+Nocbu@users.noreply.github.com>
2026-08-20 13:19:33 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int start=0;
+        int end=s.size()-1;
+        while(start<end)
+        {
+            if(!isalpha(s[start])&&!isdigit(s[start]))
+            {
+                start++;
+            }
+            else if(!isalpha(s[end])&&!isdigit(s[end]))
+            {
+                end--;
+            }
+            else if(tolower(s[start])!=tolower(s[end]))
+            {
+                return false;
+            }
+            else{
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
+};
